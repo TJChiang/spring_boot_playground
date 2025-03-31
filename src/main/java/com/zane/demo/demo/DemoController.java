891edba0a4726c6635f3fbe213e05ae59b1e6071
TJChiang <tjchiang8419@gmail.com>
2025-03-31 16:28:43 +0800

    [Feature] injecting custom application properties

--- src/main/java/com/zane/demo/demo/DemoController.java
+++ src/main/java/com/zane/demo/demo/DemoController.java
@@ -1,13 +1,25 @@
 package com.zane.demo.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
     @GetMapping("/hello")
     public String getGreeting() {
         return "Hello, World!";
+    }
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team: " + teamName;
     }
 }
