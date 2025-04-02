3bd30a99d838c051590953ae5633c5f14907d1a3
TJChiang <tjchiang8419@gmail.com>
2025-04-03 00:59:38 +0800

    reindent with space

--- src/main/java/com/zane/demo/DemoApplication.java
+++ src/main/java/com/zane/demo/DemoApplication.java
@@ -3,16 +3,11 @@ package com.zane.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(
-	scanBasePackages = {
-		"com.zane.demo",
-		"com.zane.util"
-	}
-)
+@SpringBootApplication(scanBasePackages = {"com.zane.demo", "com.zane.util"})
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
