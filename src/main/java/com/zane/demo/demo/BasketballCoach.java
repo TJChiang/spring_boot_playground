2f420ec251832c2414bbc7c038fd92a6cf5d2ce4
TJChiang <tjchiang8419@gmail.com>
2025-04-13 10:54:59 +0800

    remove PostConstruct and PreDestroy annotations

--- src/main/java/com/zane/demo/demo/BasketballCoach.java
+++ src/main/java/com/zane/demo/demo/BasketballCoach.java
@@ -1,7 +1,5 @@
 package com.zane.demo.demo;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,16 +7,6 @@ public class BasketballCoach implements Coach {
 
     public BasketballCoach() {
         System.out.println("BasketballCoach inside default constructor: " + getClass().getSimpleName());
-    }
-
-    @PostConstruct
-    public void doMyStartupStuff() {
-        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
-    }
-
-    @PreDestroy
-    public void doMyCleanupStuff() {
-        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
     }
 
     @Override
